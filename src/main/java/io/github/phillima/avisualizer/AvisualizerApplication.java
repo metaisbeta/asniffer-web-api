72ebf2a3820549cce03f0a3b3270621aad7f0edb
sergioppires <sergio.abilio.pires@gmail.com>
2021-12-12 18:09:15 -0300

    first commit

---
+++ src/main/java/io/github/phillima/avisualizer/AvisualizerApplication.java
@@ -0,0 +1,18 @@
+package io.github.phillima.avisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+@EnableAutoConfiguration
+public class AvisualizerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisualizerApplication.class, args);
+	}
+
+}
