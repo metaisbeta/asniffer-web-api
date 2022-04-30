3d948d079f74a8e2164974f669bfc4775bc30b24
sergioppires <sergio.abilio.pires@gmail.com>
2022-04-30 15:46:52 -0300

    version 1.0.0 unit tests

---
+++ src/test/java/AvisualizerApplicationTest.java
@@ -0,0 +1,11 @@
+import io.github.phillima.avisualizer.AvisualizerApplication;
+import org.junit.Test;
+
+public class AvisualizerApplicationTest {
+
+    @Test
+    public void main() {
+        AvisualizerApplication.main(new String[] {});
+    }
+
+}
